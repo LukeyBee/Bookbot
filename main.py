eaa40f9c5943b63e81167dbefda32cbc599c1bf1
LukeyBee <Ldbailey21@hotmail.com>
2025-09-24 18:22:52 +0930

    B: bookbot changes, added stats

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
-contents = ()
-def word_count(file_contents):
-    contents = file_contents.split()
-    return len(contents)
+
+from stats import word_count
+from stats import count_characters
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     count = word_count(text)
-    print(f"{count} words found in the document")
+    print(f"Found {count} total words")
+    print(count_characters(contents))
 main()
---
+++ stats.py
@@ -0,0 +1,7 @@
+contents = ()
+def word_count(file_contents):
+    contents = file_contents.split()
+    return len(contents)
+
+def count_characters(contents):
+    return contents
