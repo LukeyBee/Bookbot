5ede414afc11badd313c9615e8fc467626b0452b
LukeyBee <Ldbailey21@hotmail.com>
2025-10-08 02:31:46 +1030

    C: finished building bookbot

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-path = "books/"
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    path = sys.argv[1]
+from stats import book_splitter
+from stats import word_count
+from stats import letter_count
+from stats import dict_sort
+
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
 
-from stats import book_splitter
-from stats import word_count
-from stats import letter_count
-
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(path)
     words = book_splitter(text)
     total_words = word_count(words)
     characters = letter_count(text)
-    print(f"Found {total_words} total words")
-    print(characters)
+    book_report = dict_sort(characters)
+    print(f"""============ BOOKBOT ============
+Analyzing book found at {path}...
+----------- Word Count ----------
+Found {total_words} total words
+--------- Character Count -------""")
+    for item in book_report:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -8,8 +8,19 @@ def letter_count(book_words):
     letters_dict = {}
     words = book_words.lower()
     for letter in words:
-        if letter not in letters_dict:
-            letters_dict[letter] = 1
-        else:
-            letters_dict[letter] += 1
+        if letter.isalpha() == True:
+            if letter not in letters_dict:
+                letters_dict[letter] = 1
+            else:
+                letters_dict[letter] += 1
     return letters_dict
+
+def sort_on(items):
+    return items["num"]
+
+def dict_sort(dictionary):
+    dict_list = []
+    for char, num in dictionary.items():
+        dict_list.append({"char": char, "num": num})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
